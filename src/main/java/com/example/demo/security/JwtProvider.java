ba8bbc47bd1947e664e39b0a330d0723f61c1943
AmineElouaer <49250202+Elouaeramine@users.noreply.github.com>
2021-04-13 13:04:18 +0100

    fixed bug in generateToken

--- src/main/java/com/example/demo/security/JwtProvider.java
+++ src/main/java/com/example/demo/security/JwtProvider.java
@@ -2,9 +2,10 @@ package com.example.demo.security;
 
 
 import com.example.demo.exceptions.SpringRedditException;
-import com.example.demo.model.User;
+
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -22,7 +23,7 @@ public class JwtProvider {
         try {
             keyStore = KeyStore.getInstance("JKS");
             InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
-            keyStore.load(resourceAsStream, "secret".toCharArray());
+            keyStore.load(resourceAsStream, "secret".toCharArray());    
         } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
             throw new SpringRedditException("Exception occurred while loading keystore", e);
         }
