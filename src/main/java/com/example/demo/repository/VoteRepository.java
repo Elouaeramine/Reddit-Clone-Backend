7b5626416b208105d6fecc3a2ce0e37aad7a99f8
AmineElouaer <49250202+Elouaeramine@users.noreply.github.com>
2021-04-24 00:13:01 +0100

    added Vote Service Class

--- src/main/java/com/example/demo/repository/VoteRepository.java
+++ src/main/java/com/example/demo/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Post;
+import com.example.demo.model.User;
 import com.example.demo.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote , Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, User currentUser);
 }
--- src/main/java/com/example/demo/service/VoteService.java
+++ src/main/java/com/example/demo/service/VoteService.java
@@ -1,8 +1,50 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.VoteDto;
+import com.example.demo.exceptions.PostNotFoundException;
+import com.example.demo.exceptions.SpringRedditException;
+import com.example.demo.model.Post;
+import com.example.demo.model.Vote;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.repository.VoteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.example.demo.model.VoteType.UPVOTE;
 
 @Service
-
+@AllArgsConstructor
 public class VoteService {
+    private final VoteRepository voteRepository;
+    private final PostRepository postRepository;
+    private final AuthService authService;
+
+    @Transactional
+    public void vote(VoteDto voteDto){
+        Post post = postRepository.findById(voteDto.getPostId())
+            .orElseThrow(() -> new PostNotFoundException("Post not Found with ID " + voteDto.getPostId()));
+
+        Optional<Vote> voteByPostAndUser = voteRepository.findTopByPostAndUserOrderByVoteIdDesc(post, authService.getCurrentUser());
+        if (voteByPostAndUser.isPresent() && voteByPostAndUser.get().getVoteType().equals(voteDto.getVoteType())){
+            throw  new SpringRedditException("You have already " + voteDto.getVoteType() +"'d this post");
+        }
+        if(UPVOTE.equals(voteDto.getVoteType())){
+            post.setVoteCount(post.getVoteCount() + 1 );
+        } else {
+            post.setVoteCount(post.getVoteCount() - 1);
+        }
+        voteRepository.save(mapToVote(voteDto ,post));
+        postRepository.save(post);
+    }
+
+    private Vote mapToVote(VoteDto voteDto, Post post) {
+        return Vote.builder()
+                .voteType(voteDto.getVoteType())
+                .post(post)
+                .user(authService.getCurrentUser())
+                .build();
+    }
 }
